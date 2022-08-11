8f4d64ed345f36a1537798b6876d5bbe7ca4f199
Cameliass <97218282+Cameliasss@users.noreply.github.com>
2022-08-11 21:30:46 +0800

    DFP算法

--- main.py
+++ main.py
@@ -1,34 +1,27 @@
 '''
-    sklearn实现
+    sklearn模块实现
+    测试随机下降、牛顿、拟牛顿法
 '''
-from sklearn.ensemble import AdaBoostClassifier
 import numpy as np
+from sklearn.linear_model import LogisticRegression
 
 def main():
-    dataSet=np.array([
-        [0, 1, 3],
-        [0, 3, 1],
-        [1, 2, 2],
-        [1, 1, 3],
-        [1, 2, 3],
-        [0, 1, 2],
-        [1, 1, 2],
-        [1, 1, 1],
-        [1, 3, 1],
-        [0, 2, 1]
-
-    ])
-    Y=np.array([-1,-1,-1,-1,-1,-1,1,1,-1,-1])
-    clf=AdaBoostClassifier(learning_rate=0.5)
-    clf.fit(dataSet,Y)
-    y_predict=clf.predict(dataSet)
-    score=clf.score(dataSet,Y)
-    print('原始输出：{}'.format(Y))
-    print('原始预测值：{}'.format(y_predict))
-    print('预测的正确率：{:2f}'.format(score))
+    x_train=np.array([[3,3,3],[4,3,2],[2,1,2],[1,1,1]])
+    y_train=np.array([1,1,1,0])
+    x_new=np.array([[1, 2, -2]])
+    x_test = np.array([[-1,0,1],[2,-2,1]])
+    y_test = np.array([0,0])
+    methods=['liblinear','lbfgs','newton-cg','sag','saga']
+    res=[]
+    for method in methods:
+        clf=LogisticRegression(solver=method,max_iter=100,intercept_scaling=2)
+        clf.fit(x_train,y_train)
+        y_predict=clf.predict(x_new)
+        correct_rate=clf.score(x_test,y_test)
+        res.append((method,correct_rate))
+        print('{}方法的正确率是{}'.format(res[-1][0],res[-1][1]))
+        print('输入{}的预测值为{}'.format(x_new,y_predict))
     return
 
 if __name__=='__main__':
     main()
-
-
